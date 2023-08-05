74819db07b67fb3a84f76dc386be65a87eb2d0d4
Osman Santeliz <osanteliz88@gmail.com>
2023-08-04 21:04:14 -0600

    *Primera parte terminada, se añadio el controlador, vistas y modelo,  * se realizaron test.

---
+++ test/controllers/products_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ProductsControllerTest < ActionDispatch::IntegrationTest
+	test 'render a list of product' do
+		get products_path
+
+		assert_response :success
+		assert_select '.product', 2
+
+	end
+	
+	test 'render a detailed product page' do
+		get product_path(products(:ps4))
+
+		assert_response :success
+		assert_select '.titel', 'ps4 fat'
+		assert_select '.description', 'ps4 en buen estado'
+		assert_select '.price', '150$'
+	end	
+end	
