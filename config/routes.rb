d542139df2ed76d198983c0b930d56f00d7247fe
Osman Santeliz <osanteliz88@gmail.com>
2023-08-15 15:20:50 -0600

    * Añadiendo categorias, se creó controlador, modelos y vistas con scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   #post '/products', to: 'products#create'
   #get '/products/new', to: 'products#new', as: :new_product
   #get '/products', to: 'products#index'
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
   #patch '/products/:id', to: 'products#update'
   #delete '/products/:id', to: 'products#destroy'
 
+  resources :categories, except: :show
   resources :products, path: '/'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
