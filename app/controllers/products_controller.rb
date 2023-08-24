699c8e8fb38f22271495c045e37711f1af1a4300
Osman Santeliz <osanteliz88@gmail.com>
2023-08-24 15:23:24 -0600

    * Ordenando productos a travez de un select, por precio mas caro, mas barato y los mas recientes.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
 
 	def index
 		@categories = Category.order(name: :asc).load_async #load_async ejecuta consultas a la vez
-		@products = Product.with_attached_photo.order(created_at: :desc).load_async
+		@products = Product.with_attached_photo
 		if params[:category_id]
 			@products = @products.where(category_id: params[:category_id])
 		end	
@@ -14,7 +14,11 @@ class ProductsController < ApplicationController
 		end
 		if params[:query_text].present?
 			@products = @products.search_full_text(params[:query_text])
-		end		
+		end
+
+		order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
+
+		@products = @products.order(order_by).load_async		
 	end
 
 	def show
