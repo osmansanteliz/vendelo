fb6bc2f6626129565e6e613647c0b6e071522eca
Osman Santeliz <osanteliz88@gmail.com>
2023-08-27 17:03:13 -0600

    * refactorizando el codigo del controlador de productos, se creo una neva carpeta dentro de app, y se crearon   * las clases y metodos para las consultas en el archivo find_products.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,25 +2,8 @@ class ProductsController < ApplicationController
 
 	def index
 		@categories = Category.order(name: :asc).load_async #load_async ejecuta consultas a la vez
-		@products = Product.with_attached_photo
-		if params[:category_id]
-			@products = @products.where(category_id: params[:category_id])
-		end	
-		if params[:min_price].present?
-			@products = @products.where("price >= ?", params[:min_price])
-		end	
-		if params[:max_price].present?
-			@products = @products.where("price <= ?", params[:max_price])
-		end
-		if params[:query_text].present?
-			@products = @products.search_full_text(params[:query_text])
-		end
-
-		order_by = Product::ORDER_BY.fetch(params[:order_by]&.to_sym, Product::ORDER_BY[:newest])
-
-		@products = @products.order(order_by).load_async
-
-		@pagy, @products = pagy_countless(@products, items: 4)		
+		
+		@pagy, @products = pagy_countless(FindProducts.new.call(params).load_async, items: 4)		
 	end
 
 	def show
