b7e1f471dc3608444604270792d1fd163ed981e4
Osman Santeliz <osanteliz88@gmail.com>
2023-08-28 22:16:11 -0600

    * En esta tarea añadimos la gema bcrypt, y creamos un modelo user y un formulario para la creacion de usuarios   * se validaron los campo de email, username y password en el modelo. Se creo un namespace, y los test   * correspondientes.

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   resources :categories, except: :show
   resources :products, path: '/'
 
+  namespace :authentication, path: '', as: '' do
+    resources :users, only: [:new, :create]
+  end  
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
