f0f3879102f6e9bfdb1aaf3dba2b5035ef008614
Osman Santeliz <osanteliz88@gmail.com>
2023-08-29 22:15:22 -0600

    * Se creo el formulario para el inicio de sesion, las rutas y los test.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   namespace :authentication, path: '', as: '' do
     resources :users, only: [:new, :create]
+    resources :sessions, only: [:new, :create]
   end  
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
